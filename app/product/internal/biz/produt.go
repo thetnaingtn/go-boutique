50ad3aa686455b5e58266280fe484f3fd78d3401
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-11-19 14:36:38 +0700

    refactor: use proper name

--- app/product/internal/biz/produt.go
+++ app/product/internal/biz/produt.go
@@ -3,21 +3,8 @@ package biz
 import (
 	"context"
 
-	v1 "github.com/thetnaingtn/go-boutique/api/helloworld/v1"
-
-	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 )
-
-var (
-	// ErrUserNotFound is user not found.
-	ErrUserNotFound = errors.NotFound(v1.ErrorReason_USER_NOT_FOUND.String(), "user not found")
-)
-
-// Greeter is a Greeter model.
-type Greeter struct {
-	Hello string
-}
 
 type Product struct {
 	Id          string `db:"id"`
@@ -26,7 +13,6 @@ type Product struct {
 	Price       int64  `db:"price"`
 }
 
-// GreeterRepo is a Greater repo.
 type ProductRepo interface {
 	Save(context.Context, *Product) (*Product, error)
 	Update(context.Context, *Product) (*Product, error)
@@ -35,18 +21,15 @@ type ProductRepo interface {
 	ListAll(context.Context) ([]*Product, error)
 }
 
-// ProductUsecase is a Greeter usecase.
 type ProductUsecase struct {
 	repo ProductRepo
 	log  *log.Helper
 }
 
-// NewGreeterUsecase new a Greeter usecase.
 func NewProductUsecase(repo ProductRepo, logger log.Logger) *ProductUsecase {
 	return &ProductUsecase{repo: repo, log: log.NewHelper(logger)}
 }
 
-// CreateGreeter creates a Greeter, and returns the new Greeter.
 func (uc *ProductUsecase) CreateProduct(ctx context.Context, g *Product) (*Product, error) {
 	// uc.log.WithContext(ctx).Infof("CreateGreeter: %v", g.Hello)
 	return uc.repo.Save(ctx, g)
